bce2d79f670d4a9309db2a8e72fe45e2071fd6cb
azharkova <azharkova@usetech.ru>
2020-10-03 22:06:49 +0700

    fix network+ WIP

---
+++ news-mvi/news-mvi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  news-mvi
+//
+//  Created by Anna Zharkova on 03.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ news-mvi/news-mvi/news_mviApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_mviApp.swift
+//  news-mvi
+//
+//  Created by Anna Zharkova on 03.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct news_mviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewsListView()
+        }
+    }
+}
